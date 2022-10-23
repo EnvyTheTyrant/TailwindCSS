21e6ddb7f8e4ab58023040dbd40a4ec0a26361d8
Dulguun <dulguun.web@gmail.com>
2022-10-23 22:25:52 +0800

    third commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}"],
+  content: [],
   theme: {
     extend: {},
+  },
+  variants: {
+    extend: {
+      backgroundColor: ["responsive", "hover", "focus", "active"],
+    },
   },
   plugins: [],
 };
